d7380986353ccf6b20ce9d1a4938a1c316dfff8d
Vishesh Gupta <visheshguptavgr@gmail.com>
2023-07-28 12:11:36 +0000

    added gimme comments integration

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,16 @@
-import React from "react"
-import { Container } from "react-bootstrap";
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <>
-            <footer className=" bg-primary">
-                <Container fluid="xxl" className="p-5 d-flex flex-column justify-content-center align-items-center">
-                    <div className="wrapper">
-                        {/* <div className="button-social" onClick={() => window.open("https://www.instagram.com/vishesh22_17/")}>
+  return (
+    <>
+      <footer className="" style={{ background: '#506AD4' }}>
+        <Container
+          fluid="xxl"
+          className="p-5 d-flex flex-column justify-content-center align-items-center"
+        >
+          <div className="wrapper">
+            {/* <div className="button-social" onClick={() => window.open("https://www.instagram.com/vishesh22_17/")}>
                             <div className="icon">
                                 <i className="fab fa-instagram"></i>
                             </div>
@@ -21,21 +24,28 @@ const Footer = () => {
                             <span>Linkedin</span>
                         </div> */}
 
-                        <div className="button-social" onClick={() => window.open("https://github.com/VisheshVGR/Hack_The_Web")}>
-                            <div className="icon">
-                                <i className="fab fa-github"></i>
-                            </div>
-                            <span>Github</span>
-                        </div>
-                    </div>
+            <div
+              className="button-social"
+              onClick={() =>
+                window.open('https://github.com/VisheshVGR/Hack_The_Web')
+              }
+            >
+              <div className="icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <span>Github</span>
+            </div>
+          </div>
 
-                    <p className="text-white pt-4 text-center">
-                        <i className="fas fa-code"></i> Hack The Web<br />2022 &#169; All Rights Reserved.
-                    </p>
-                </Container>
-            </footer>
-        </>
-    )
-}
+          <p className="text-white pt-4 text-center">
+            <i className="fas fa-code"></i> Hack The Web
+            <br />
+            2022 &#169; All Rights Reserved.
+          </p>
+        </Container>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
